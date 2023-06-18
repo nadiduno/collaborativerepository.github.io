4167638fc8b83149b534b716f292ab129db8c63d
Nadi Duno <nadiduno@gmail.com>
2023-06-18 19:22:59 +0000

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function getGitHubProfileInfos() {
+    fetch('https://api.kanye.rest')
+      .then(response => response.json())
+      .then(data => {
+        textRandom.textContent = data.quote
+      })
+}
+  
+getGitHubProfileInfos()
